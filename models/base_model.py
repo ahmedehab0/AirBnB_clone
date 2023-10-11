c3535c6832ee431fc37b055cd1c60a267cf7c389
ahmedehab0 <ahmed.eh01@gmail.com>
2023-10-10 21:20:37 -0400

    file storage

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,6 @@
 import datetime
 import uuid
+import models
 
 class BaseModel:
     """base model that defines all common attributes
@@ -26,6 +27,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.datetime.now()
             self.updated_at = datetime.datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """return represantation of the class"""
@@ -34,6 +36,7 @@ class BaseModel:
     def save(self):
         """ updates the public instance attribute updated_at with the current datetime"""
         self.updated_at = datetime.datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """returns a dictionary containing all keys/values of __dict__ of
